2f908655f917197cbb683dc3003c2ea87df9c1e7
tichi <timmy.g.chi@gmail.com>
2017-11-25 16:06:02 -0800

    pushing warning-alert css and declaring component in module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './practice/warning-alert/warning-alert.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule
